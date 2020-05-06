69719af2d86b836f922d60d8116c7590502899aa
Jacobo Hernandez <jacobolhernandez@gmail.com>
2020-05-06 16:38:15 -0500

    Initial Commit. Added base UIStackView Layout

--- TinderClone/ContentView.swift
+++ TinderClone/ContentView.swift
@@ -6,16 +6,44 @@
 //  Copyright © 2020 Jacobo Hernandez. All rights reserved.
 //
 
-import SwiftUI
+import UIKit
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
-}
+class ViewController: UIViewController {
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let grayView = UIView()
+        grayView.backgroundColor = .gray
+        
+        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subviews)
+        topStackView.distribution = .fillEqually
+        
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+//        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        
+        
+        view.addSubview(stackView)
+        stackView.frame = .init(x: 0, y: 0, width: 300, height: 200)
+        
+        stackView.fillSuperview()
+        
     }
+
+
 }
